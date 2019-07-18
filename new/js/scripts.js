e628fbc5f78e62c44a017ecefade3f96e7a9dd22
htmltest <struev.vs@gmail.com>
2019-07-18 16:37:31 +0300

    update

--- new/js/scripts.js
+++ new/js/scripts.js
@@ -5,4 +5,12 @@ $(document).ready(function() {
         e.preventDefault();
     });
 
+    $('.kr-score-radio').click(function() {
+        var curRadio = $(this);
+        var curBlock = curRadio.parent().parent();
+        curBlock.find('.kr-score-radio.active').removeClass('active');
+        curRadio.addClass('active');
+        curBlock.find('input').val(curRadio.text()).trigger('change');
+    });
+
 });
